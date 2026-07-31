6108cf52c55b1009971036495ca99f8f7f0b60cd
baiching <bachmarma@gmail.com>
2026-08-01 00:21:10 +0200

    update: added one more api

--- src/include/common/malloc_mi.hpp
+++ src/include/common/malloc_mi.hpp
@@ -15,3 +15,5 @@
 #define strndup(str, n)					 mi_strndup(str, n)
 #define aligned_alloc(align, size)		 mi_aligned_alloc(align, size)
 #define posix_memalign(ptr, align, size) mi_posix_memalign(ptr, align, size)
+#define malloc_aligned(size, alignment)	 mi_malloc_aligned(size, alignment)
+#define realloc_aligned(p, newsize, alignment) mi_realloc_aligned(((p), (newsize), (alignment))
