6ced10b989ae6f7754d4d0839afba18fd9b0b285
baiching <bachmarma@gmail.com>
2026-08-03 01:32:55 +0200

    add: a thread-safe wraper around unordered_map

---
+++ src/include/data_structures/safe_map.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <unordered_map>
+#include <mutex>
+#include <shared_mutex>
+#include <optional>
+#include <vector>
+#include <functional>
+
+namespace rgdb {
+
+	template<typename Key, typename Value>
+	class SafeHashMap {
+	private:
+		std::unordered_map<Key, Value> map;
+		std::shared_mutex lock;
+
+	public:
+		SafeHashMap();
+
+		void insert(Key& key, const Value& value);
+		void insert(Key&& key, Value&& value);
+
+		template<typename... Args>
+		void emplace(Args... args);
+
+		bool try_insert(Key& key, const Value& value);
+
+		std::optional<Value> get(Key& key);
+		bool erase(Key& key);
+		bool contains(Key& key);
+		size_t size();
+		bool empty();
+		void clear();
+		std::vector<Key> get_all_keys() const;
+		std::vector<Key> get_all_values() const;
+
+		// read-only to iterate
+		void for_each(std::function<void(const Key& key, const Value& value)>) const;
+
+		// iterate: read and write both
+		void for_each_mut(std::function<void(Key&, Value&)>);
+
+		void reserve(size_t size);
+
+		void rehash(size_t size);
+
+		// swap with another SafeHashMap
+		void swap(SafeHashMap& other) noexcept;
+	};
+}
